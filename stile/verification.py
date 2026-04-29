8d5a2ba782578058202c0badc620316d44f51f24
Sasha Krassovsky <krassovskysasha@gmail.com>
2026-04-28 18:59:50 -0700

    Remove Rust dependency!

--- stile/verification.py
+++ stile/verification.py
@@ -5,55 +5,6 @@ from dataclasses import dataclass, field
 from .type import *
 from .indexing import Domain, AffineConstraint, interval_domain, ge, lt, and_domains, simplify_domain
 from .frozen_counter import FrozenCounter
-from ._rust import RustEgraph, RustExpr # ty: ignore
-
-def expr_type_to_rust_expr(expr : ExprType) -> RustExpr:
-    match expr:
-        case Constant(v):
-            return RustExpr.Constant(v)
-        case Tensor(dims):
-            return RustExpr.Tensor([dim_name(d) for d in dims])
-        case UnaryOp(op, child):
-            rust_child = expr_type_to_rust_expr(child)
-            match op:
-                case "exp":
-                    return RustExpr.Exp(rust_child)
-                case "sin":
-                    return RustExpr.Sin(rust_child)
-                case "cos":
-                    return RustExpr.Cos(rust_child)
-                case "sqrt":
-                    return RustExpr.Sqrt(rust_child)
-            raise ValueError(f"Unknown unary op {op}")
-        case BinaryOp(op, lhs, rhs):
-            rust_lhs = expr_type_to_rust_expr(lhs)
-            rust_rhs = expr_type_to_rust_expr(rhs)
-            match op:
-                case "+":
-                    return RustExpr.Add(rust_lhs, rust_rhs)
-                case "-":
-                    return RustExpr.Sub(rust_lhs, rust_rhs)
-                case "*":
-                    return RustExpr.Mul(rust_lhs, rust_rhs)
-                case "/":
-                    return RustExpr.Div(rust_lhs, rust_rhs)
-                case "max":
-                    return RustExpr.BinaryMax(rust_lhs, rust_rhs)
-            raise ValueError(f"Unknown binary op {op}")
-        case Repeat(dim, child):
-            d = dim_name(dim)
-            rust_child = expr_type_to_rust_expr(child)
-            return RustExpr.Repeat(d, rust_child)
-        case Reduce(op, dim, child):
-            d = dim_name(dim)
-            start, end = dim_start(dim), dim_end(dim)
-            rust_child = expr_type_to_rust_expr(child)
-            match op:
-                case "sum":
-                    return RustExpr.Sum(d, start, end, rust_child)
-                case "max":
-                    return RustExpr.Max(d, start, end, rust_child)
-            raise ValueError(f"Unknown reduction op {op}")
 
 
 @dataclass(frozen=True)
